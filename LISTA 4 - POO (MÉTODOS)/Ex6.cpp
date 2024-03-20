4963adba81c24b81eec64495bda774420490d37f
Gabriel Rigoni <139931848+gabrielrigonidev@users.noreply.github.com>
2024-03-20 13:51:52 -0300

    Adicionando Ex 6

---
+++ LISTA 4 - POO (MÉTODOS)/Ex6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int soma (int a, int b);
+int quad (int c);
+int somaquad (int d, int e);
+
+int main(){
+    int x, y;
+    cout << "Digite: " << endl;
+    cin >> x >> y;
+    cout << "Soma dos quadrados dos números: " << somaquad(x,y) << endl;
+    return 0;
+}
+
+    int soma (int a, int b){
+        return a+b;
+    }
+    
+    int quad (int c){
+        return c*c; 
+    }
+    
+    int somaquad (int d, int e){
+        return soma(quad(d), quad(e));
+    }
